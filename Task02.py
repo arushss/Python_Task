53c5e81c01221d86f6380a875213e644b6b46049
unknown <arushss@gmail.com>
2020-07-01 10:40:55 -0400

    changed the filenames to be more meaningful

--- Task02.py
+++ Task02.py
@@ -67,3 +67,27 @@ if (age >= 11):
 else:
     print("You are not eligible to buy a ticket.")
 
+# Section 4 of Task 2 in python
+#Write a program in Python to break and continue if the following cases occur:
+#If a user enters a negative number just break the loop and print “It’s Over”
+#If a user enters a positive number just continue in the loop and print “Good Going”
+
+while True:
+    print("Enter any number")
+    x=eval(input())
+    if(x<0):
+        print("It's Over")
+        break
+    print("Good Going")
+    continue
+
+# Section 5 of Task 2 in python
+#Write a program in Python which will find all such numbers which are divisible by 7 but are not a multiple of 5, 
+#between 2000 and 3200.
+
+x=2000
+while x<=3200:
+    if(x%7==0 and x%5!=0):
+        print(x)
+    x+=1
+
