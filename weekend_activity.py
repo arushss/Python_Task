3a6284737987d2c527a35f9e134a5bbb05394453
unknown <arushss@gmail.com>
2020-07-21 03:31:50 -0400

    completed all the required task

--- weekend_activity.py
+++ weekend_activity.py
@@ -22,3 +22,82 @@ print(x[5][5][:1])
 print(x[9:])
 
 # Section 4
+b=list(range(1,1000))
+b=list(xrange(1,1000))
+# Return type of range is list whereas return type of xrange is xrange object
+# range occupies more size compared to xrange hence xrange is faster to implement
+
+# Section 5
+# Since tuples are immutable type compared to lists, they can be used as a dictionary in various applications.
+
+# Section 6
+for i in range(1,100):
+    if i%6==0:
+        print(i)
+
+# Section 7
+print("Enter any string")
+name=input()
+reverse=""
+print("The original string is {}".format(name))
+length=len(name)
+i=length-1
+while i>=0:
+    reverse+=name[i]
+    i-=1
+print("Printing only the vowel alphabet in reversed string")
+for position,i in enumerate(reverse):
+    if i == 'a' or i == 'e' or i == 'i' or i == 'o' or i =='u':
+        print(i,"found at index:", position)
+
+# Section 8
+string = "hello my name is Abcde"
+input=[]
+input.extend(string.split())
+print(input)
+for i in range(len(input)):
+    if len(input[i]) % 2 == 0:
+        print(input[i])
+
+# Section 9
+x=[1,2,3,4,5,6,7,8,9,-1]
+for i in x:
+    for j in x:
+        if i+j==8:
+            print("the pair of numbers are: {} {}".format(i,j))
+
+# Section 10
+even_list=[]
+odd_list=[]
+print("Enter the number in the range of 1 to 50")
+x=eval(input())
+while len(even_list) <= 5 and len(odd_list) <= 5:
+    if x%2==0:
+        even_list.append(x)
+    else:
+        odd_list.append(x)
+    x=eval(input())
+print("Sum of even lists are: ", sum(even_list), "and a max value is: ", max(even_list))
+print("Sum of odd lists are: ", sum(odd_list), "and a max value is: ", max(odd_list))     
+
+# Section 11
+dictionary={}
+print("Enter any random string")
+name=input()
+for i in range(len(name)):
+    if name[i].isdigit():
+        continue
+    if name[i] in dictionary:
+        dictionary[name[i]] +=1
+    else:
+        dictionary[name[i]] = 1
+print(dictionary)
+
+# Section 12
+x=(1,2,3,4,5,6,7,8,9,10)
+y=[]
+for i in x:
+    if i%2==0:
+        y.append(i)
+answer=tuple(y)
+print(answer)
