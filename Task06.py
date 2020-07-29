00cce72767c29527c720b82b0474267e44412f63
unknown <arushss@gmail.com>
2020-07-28 22:09:21 -0400

    answer 2

--- Task06.py
+++ Task06.py
@@ -6,3 +6,8 @@ except NameError:
     print("maybe some variable is uninitialized")
 
 # Section 2
+try:
+    a=b+5
+    print(a)
+except NameError:
+    print("maybe some variable is uninitialized")
